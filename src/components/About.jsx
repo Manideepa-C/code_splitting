6d3c210f7ddfe9204f89aa2754584278d42b7300
C Naga Manideepa <manideepa@jirehsol.com>
2024-12-10 10:05:59 +0530

    practice branch changes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,7 +7,7 @@ const About = () => {
             <Navbar />
             <section className="hero-section">
                 <h1>The About Page</h1>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, saepe? Molestias quae deleniti officia fugit mollitia commodi atque exercitationem sequi vel placeat adipisci itaque aliquam autem, et ut eos iusto error, sint dolor. Blanditiis obcaecati nobis ipsam eum unde vel, esse quia sapiente, voluptas maxime animi iste. In, voluptatibus! Minima?</p>
+                <p>Lorem lorem ipsum ipsum dolor sit amet consectetur adipisicing elit. Veniam, saepe? Molestias quae deleniti officia fugit mollitia commodi atque exercitationem sequi vel placeat adipisci itaque aliquam autem, et ut eos iusto error, sint dolor. Blanditiis obcaecati nobis ipsam eum unde vel, esse quia sapiente, voluptas maxime animi iste. In, voluptatibus! Minima?</p>
             </section>
         </>
     )
--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -7,7 +7,7 @@ const Service = () => {
             <Navbar />
             <section className="hero-section">
                 <h1>The Service Page</h1>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, saepe? Molestias quae deleniti officia fugit mollitia commodi atque exercitationem sequi vel placeat adipisci itaque aliquam autem, et ut eos iusto error, sint dolor. Blanditiis obcaecati nobis ipsam eum unde vel, esse quia sapiente, voluptas maxime animi iste. In, voluptatibus! Minima?</p>
+                <p>Lorem ipsum isum dolor sit amet consectetur adipisicing elit. Veniam, saepe? Molestias quae deleniti officia fugit mollitia commodi atque exercitationem sequi vel placeat adipisci itaque aliquam autem, et ut eos iusto error, sint dolor. Blanditiis obcaecati nobis ipsam eum unde vel, esse quia sapiente, voluptas maxime animi iste. In, voluptatibus! Minima?</p>
             </section>
         </>
     )
